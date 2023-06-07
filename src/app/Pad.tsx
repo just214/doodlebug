2a02761242529a99b2c44b211b6fc50dbe4d9018
just214 <21136155+just214@users.noreply.github.com>
2023-06-07 15:14:55 -0400

    refactor: refinements for pad page

--- src/app/Pad.tsx
+++ src/app/Pad.tsx
@@ -12,7 +12,7 @@ const styles = {
 };
 
 const buttonClassNames =
-  "rounded-full p-2 transition-colors duration-200 ease-in-out";
+  "rounded-full p-2 transition-all active:scale-95 duration-200 ease-in-out";
 
 export const Pad = () => {
   const [eraserMode, setEraserMode] = useState<boolean>(false);
@@ -25,22 +25,21 @@ export const Pad = () => {
     });
   }
   return (
-    <div className="flex flex-col min-h-screen h-screen  w-full bg-gray-900 p-2 md:p-4">
-      <div className="w-full flex items-center justify-center mb-3">
+    <div className="h-screen w-full bg-gray-900">
+      <div className="fixed top-4 w-full flex items-center justify-center mb-3">
         <Header />
       </div>
 
-      <div className="rounded-xl overflow-hidden flex-1 z-0">
-        <ReactSketchCanvas
-          style={styles}
-          ref={ref}
-          strokeWidth={4}
-          eraserWidth={20}
-          height="100%"
-          strokeColor="#fef9c3"
-          canvasColor="#0c0a09"
-        />
-      </div>
+      <ReactSketchCanvas
+        style={styles}
+        ref={ref}
+        strokeWidth={4}
+        eraserWidth={20}
+        height="100%"
+        strokeColor="#fef9c3"
+        canvasColor="#0c0a09"
+      />
+
       <div className="fixed bottom-8 left-10 right-10 text-center z-[9999999]">
         <div className="inline-flex items-center justify-center gap-4 text-4xl mt-4 rounded-full p-3 bg-gray-500/20">
           <button
